84446e1b4d45b8f5c058ef1e12720091e6bb46a0
ilya osovets <iosovets@gmail.com>
2016-04-21 11:19:43 -0700

    updated defaults

--- controllers/questions_controller.js
+++ controllers/questions_controller.js
@@ -1,11 +1,38 @@
 var Question = require('../models/question');
-var questions = {}
+var questions = {};
 
 questions.index = function(req, res) {
   Question.find({}, function(err, questions) {
     if (err) return res.json(err);
     res.json(questions);
   });
+};
+
+questions.create = function(req, res) {
+var question = new Question();
+
+
+if(req.body.title === " "){
+  res.json({sucess:false,message:'Post not created! pow'});
+question.title = req.body.title;
 }
+
+
+if(req.body.body === " "){
+  question.body =  "whatsupwiddatass?";
+}else{
+question.body = req.body.body;
+}
+
+console.log(question);
+question.save(function(err, questions) {
+    if (err) {
+      return res.json(err);
+    }
+    res.json(questions);
+  });
+};
+
+
 
 module.exports = questions;
--- routes/questions_router.js
+++ routes/questions_router.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var questions = require('../controllers/questions_controller');
 
 router.get('/', questions.index);
+router.post('/', questions.create);
+
 
 module.exports = router;
